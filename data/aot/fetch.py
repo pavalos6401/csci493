38ac91d24b9fb513392edaa13a4eeaed3042e935
Pedro Avalos <pavalos6401@gmail.com>
2021-10-07 11:59:08 -0500

    add command-line arguments and total timing

--- data/aot/fetch.py
+++ data/aot/fetch.py
@@ -1,12 +1,30 @@
+import argparse
 import requests
 import json
 import pandas as pd
 from time import time
 
 
-# Start time for the data retrieval
-start_data: str = "-1h"
-print(f"Retrieving data using {start_data} as start")
+start_total: float = time()
+# Command-line arguments for the script
+parser = argparse.ArgumentParser(description="Retrieve Array of Things data.")
+parser.add_argument(
+    "--start",
+    metavar="S",
+    type=str,
+    required=False,
+    default="-1h",
+    help="start time for data retrieval filter",
+)
+parser.add_argument(
+    "--name",
+    metavar="D",
+    type=str,
+    required=False,
+    default="env.temperature",
+    help="name of data to retrieve, e.g. env.temperature",
+)
+args = parser.parse_args()
 
 # URL to retrieve the Array of Things data
 url: str = "https://data.sagecontinuum.org/api/v1/query"
@@ -18,13 +36,14 @@ headers: dict[str, str] = {
 
 # Filters for the data
 data: dict[str, str] = {
-    "start": start_data,
+    "start": args.start,
     "filter": {
-        "name": "env.temperature"
+        "name": args.name
     }
 }
 
 # Retrieve the data
+print(f"Retrieving {args.name} data using {args.start} as start")
 start: float = time()
 print()
 response: requests.Response = requests.post(
@@ -53,7 +72,7 @@ for i in split_text:
     for key in ["host", "job", "node", "plugin", "sensor", "task", "vsn"]:
         j[key] = j["meta"][key]
     j.pop("meta", None)
-    j["temp"] = j["value"]
+    j[args.name] = j["value"]
     j.pop("name", None)
     j.pop("value", None)
     data_list.append(j)
@@ -64,3 +83,5 @@ print(f"Time to clean data: {end - start}sec")
 df: pd.DataFrame = pd.DataFrame(data_list)
 # Dump the table into a csv file
 df.to_csv("data.csv", index=False)
+end_total: float = time()
+print(f"Total time: {end_total - start_total}sec")
