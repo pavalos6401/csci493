c3f50449742f0b4baab870f2ffa8e4f65c287c70
Pedro Avalos <pavalos6401@gmail.com>
2021-11-09 08:32:07 -0600

    formatting and minor changes

--- data/aot/fetch_tar.py
+++ data/aot/fetch_tar.py
@@ -2,11 +2,13 @@
 
 """Request data from the array of things archive/historical data."""
 
+
 from argparse import ArgumentParser
 import tarfile
 from time import time
 import pandas as pd
 import requests
+
 
 DATA_CHOICES = [
     "temp2",
@@ -44,13 +46,12 @@ print(f"store to: {TARGET_PATH}")
 
 start = time()
 response = requests.get(URL, stream=True)
-print(response)
+print(f"status code: {response.status_code}")
 
 if response.status_code == 200:  # 200 means the request succeeded
     # Write the tar (compressed file)
     with open(TARGET_PATH, "wb") as f:
         f.write(response.raw.read())
-
     end = time()
     print(f"Time to get compressed file: {end - start}sec")
 
@@ -58,17 +59,19 @@ if response.status_code == 200:  # 200 means the request succeeded
     with tarfile.open(TARGET_PATH) as tar:
         tar.extractall(".")
     end = time()
-    print(f"Time to uncompress file: {end - start}sec");
+    print(f"Time to uncompress file: {end - start}sec")
 
     start = time()
     reader = pd.read_csv(f"AoT_Chicago.complete.{args.data}/data.csv.gz",
                          compression="gzip", parse_dates=True, chunksize=10000)
-
-    selected = pd.read_csv("selected_nodes.csv", index_col=0, squeeze=True).to_dict()
+    # Dictionary of node_id to vsn
+    selected = pd.read_csv("selected_nodes.csv",
+                           index_col=0, squeeze=True).to_dict()
     for chunk_df in reader:
         chunk_df = chunk_df.drop(columns=["subsystem", "sensor", "parameter"])
         result_df = chunk_df[chunk_df["node_id"].isin(selected.keys())]
         result_df["vsn"] = result_df["node_id"].map(selected)
-        result_df.to_csv(f"AoT_Chicago.complete.{args.data}/data.csv", mode="a")
+        result_df.to_csv(f"AoT_Chicago.complete.{args.data}/data.csv",
+                         mode="a")
     end = time()
     print(f"Time to clean data: {end - start}sec")
