c6b0389ed9c35e65d60af17385853012e786b037
Pedro Avalos <pavalos6401@gmail.com>
2021-11-10 16:43:05 -0600

    add optional download skip

--- data/aot/fetch_tar.py
+++ data/aot/fetch_tar.py
@@ -8,6 +8,7 @@ import tarfile
 from time import time
 import pandas as pd
 import requests
+from os.path import exists
 
 
 DATA_CHOICES = [
@@ -30,59 +31,68 @@ parser.add_argument(
     default=DATA_CHOICES[0],
     help=f"data to retrieve, options: {DATA_CHOICES}, default: {DATA_CHOICES[0]}",
 )
+parser.add_argument(
+    "--skip",
+    action="store_true",
+    required=False,
+    help="skip the download and uncompressing if the folder exists",
+)
 args = parser.parse_args()
 
+FOL_PATH = f"AoT_Chicago.complete.{args.data}"
 URL = "http://www.mcs.anl.gov/research/projects/waggle/downloads/datasets/"
-TARGET_PATH = ""
+TAR_PATH = ""
 if args.data == "latest":
-    URL += f"AoT_Chicago.complete.{args.data}.tar"
-    TARGET_PATH = f"{args.data}.tar"
+    URL += f"{FOL_PATH}.tar"
+    TAR_PATH = f"{args.data}.tar"
 else:
-    URL += f"slices/AoT_Chicago.complete.{args.data}.tar.gz"
-    TARGET_PATH = f"{args.data}.tar.gz"
+    URL += f"slices/{FOL_PATH}.tar.gz"
+    TAR_PATH = f"{args.data}.tar.gz"
 
-print(f"url: {URL}")
-print(f"store to: {TARGET_PATH}")
+if not (exists(f"./{FOL_PATH}/data.csv.gz") and args.skip):
+    print(f"url: {URL}")
+    print(f"store to: {TAR_PATH}")
 
-print("requesting compressed file...")
+    print("requesting compressed file...")
+    start = time()
+    response = requests.get(URL, stream=True)
+    print(f"status code: {response.status_code}")
+
+    if response.status_code == 200:  # 200 means the request succeeded
+        with open(TAR_PATH, "wb") as f:
+            f.write(response.raw.read())
+        end = time()
+        print(f"time to get compressed file: {end - start}sec")
+
+        print("uncompressing file...")
+        start = time()
+        with tarfile.open(TAR_PATH) as tar:
+            tar.extractall(".")
+        end = time()
+        print(f"time to uncompress file: {end - start}sec")
+
+print("cleaning data...")
 start = time()
-response = requests.get(URL, stream=True)
-print(f"status code: {response.status_code}")
-
-if response.status_code == 200:  # 200 means the request succeeded
-    with open(TARGET_PATH, "wb") as f:
-        f.write(response.raw.read())
-    end = time()
-    print(f"time to get compressed file: {end - start}sec")
-
-    print("uncompressing file...")
-    start = time()
-    with tarfile.open(TARGET_PATH) as tar:
-        tar.extractall(".")
-    end = time()
-    print(f"time to uncompress file: {end - start}sec")
-
-    print("cleaning data...")
-    start = time()
-    reader = pd.read_csv(f"AoT_Chicago.complete.{args.data}/data.csv.gz",
-                         compression="gzip", parse_dates=True, chunksize=10000)
-    # Dictionary of node_id to vsn
-    selected = pd.read_csv("selected_nodes.csv", index_col=0, squeeze=True,
-                           usecols=["node_id", "vsn"]).to_dict()
-    lat_dict = pd.read_csv(f"AoT_Chicago.complete.{args.data}/nodes.csv",
-                           usecols=["vsn", "lat"], index_col=0,
-                           squeeze=True).to_dict()
-    lon_dict = pd.read_csv(f"AoT_Chicago.complete.{args.data}/nodes.csv",
-                           usecols=["vsn", "lon"], index_col=0,
-                           squeeze=True).to_dict()
-    for chunk_df in reader:
-        chunk_df = chunk_df.drop(
-            columns=["subsystem", "sensor", "parameter", "value_raw"])
-        result_df = chunk_df[chunk_df["node_id"].isin(selected.keys())]
-        result_df["vsn"] = result_df["node_id"].map(selected)
-        result_df["lat"] = result_df["vsn"].map(lat_dict)
-        result_df["lon"] = result_df["vsn"].map(lon_dict)
-        result_df.to_csv(f"AoT_Chicago.complete.{args.data}/data.csv",
-                         mode="a", header=False)
-    end = time()
-    print(f"time to clean data: {end - start}sec")
+reader = pd.read_csv(f"{FOL_PATH}/data.csv.gz",
+                     compression="gzip", parse_dates=True, chunksize=10000)
+# Dictionary of node_id to vsn
+selected = pd.read_csv("selected_nodes.csv", index_col=0, squeeze=True,
+                       usecols=["node_id", "vsn"]).to_dict()
+lat_dict = pd.read_csv(f"AoT_Chicago.complete.{args.data}/nodes.csv",
+                       usecols=["vsn", "lat"], index_col=0,
+                       squeeze=True).to_dict()
+lon_dict = pd.read_csv(f"AoT_Chicago.complete.{args.data}/nodes.csv",
+                       usecols=["vsn", "lon"], index_col=0,
+                       squeeze=True).to_dict()
+for chunk_df in reader:
+    chunk_df = chunk_df.drop(
+        columns=["subsystem", "sensor", "parameter", "value_raw"]
+    )
+    result_df = chunk_df[chunk_df["node_id"].isin(selected.keys())]
+    result_df["vsn"] = result_df["node_id"].map(selected)
+    result_df["lat"] = result_df["vsn"].map(lat_dict)
+    result_df["lon"] = result_df["vsn"].map(lon_dict)
+    result_df.to_csv(f"AoT_Chicago.complete.{args.data}/data.csv",
+                     mode="a", header=False)
+end = time()
+print(f"time to clean data: {end - start}sec")
