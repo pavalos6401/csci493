93bb52e73d5c58f77df6e68afaf013ff82ecf7dd
Pedro Avalos <pavalos6401@gmail.com>
2021-11-30 03:46:58 +0000

    fix combine script

--- data/aot/combine.py
+++ data/aot/combine.py
@@ -18,21 +18,24 @@ def vsn_step(d, vsn, nodes_meta, vsn_df, df):
     Params:
         d (datetime.datetime): Date.
         vsn (str): Node number.
-        df (pd.DataFrame): DataFrame of all data.
+        nodes_meta (pd.DataFrame): Node metadata.
+        vsn_df (pd.DataFrame): Node data.
+        df (pd.DataFrame): Output DataFrame of all data.
+
+    Returns:
+        (pd.DataFrame): The output dataframe, with the current row appended.
     """
 
-    row = pd.Series(
-        [
-            d,
-            vsn,
-            vsn_df.loc[df["date"] == d]["data"],
-            nodes_meta.loc[nodes_meta["vsn"] == vsn]["lat"],
-            nodes_meta.loc[nodes_meta["vsn"] == vsn]["lon"],
-        ],
-        index=df.columns
-    )
+    temp = vsn_df.loc[vsn_df["date"] == d]["data"]
+    row = {
+        "date": d,
+        "vsn": vsn,
+        "temp": temp.iloc[-1],
+        "lat": nodes_meta.at[nodes_meta.index[0], "lat"],
+        "lon": nodes_meta.at[nodes_meta.index[0], "lon"],
+    }
 
-    df.append(row, ignore_index=True, inplace=True)
+    return df.append(row, ignore_index=True)
 
 
 def main():
@@ -48,18 +51,25 @@ def main():
     # Date range used in the normalize script
     date_rng = pd.date_range(
         start=args_df.iat[0, 1], end=args_df.iat[0, 2], freq=args_df.iat[0, 3]
-    )
+    ).tolist()
     # Metadata for all nodes being used
     nodes_meta = pd.read_csv("selected_nodes.csv")
     vsns = nodes_meta["vsn"].tolist()
 
     vsn_dfs = dict()
     for vsn in vsns:
-        vsn_dfs[vsn] = pd.read_csv(f"nodes/node-{vsn}.csv")
+        tmp = pd.read_csv(f"nodes/node-{vsn}.csv")
+        tmp["date"] = pd.to_datetime(
+            tmp["date"], format="%Y-%m-%d %H:%M:%S"
+        )
+        vsn_dfs[vsn] = tmp
+
 
     for d in date_rng:
+        print(f"Date {d}... ", end="")
         for vsn in vsns:
-            vsn_step(d, vsn, nodes_meta, vsn_dfs[vsn], df)
+            df = vsn_step(d, vsn, nodes_meta[nodes_meta["vsn"] == vsn], vsn_dfs[vsn], df)
+        print("Done")
 
     df.to_csv("aot-data.csv")
 
